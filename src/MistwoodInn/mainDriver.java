3ad58c2ce3abdb5f59b54d4b228a43d80a08494e
kollmi <e.kollmer11@gmail.com>
2022-04-11 00:42:32 -0400

    error handling

--- src/MistwoodInn/mainDriver.java
+++ src/MistwoodInn/mainDriver.java
@@ -1,6 +1,7 @@
 package MistwoodInn;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class mainDriver {
@@ -11,47 +12,65 @@ public class mainDriver {
         System.out.println(narrativeHost.getNarrative("intro1"));
         System.out.println(narrativeHost.getNarrative("intro2"));
         System.out.println("Do you go back outside to look for the old man? (Enter Yes or No)");
-        String oldManAnswer = input.nextLine();
-        //should implement try catch here
-        if (oldManAnswer.equals("Yes")){
-            System.out.println(narrativeHost.getNarrative("oldManCheck"));
-            inventory.add("Silver coin");
-            //System.out.println(inventory.contains("Silver coin"));
-            System.out.println(narrativeHost.getNarrative("enterInn"));
-        }
-        else if (oldManAnswer.equals("No")){
-            System.out.println(narrativeHost.getNarrative("enterInn"));
+
+        while(true){
+            String oldManAnswer = input.nextLine();
+            if (oldManAnswer.equals("Yes")) {
+                System.out.println(narrativeHost.getNarrative("oldManCheck"));
+                inventory.add("Silver coin");
+                //System.out.println(inventory.contains("Silver coin"));
+                System.out.println(narrativeHost.getNarrative("enterInn"));
+                break;
+            }
+            else if (oldManAnswer.equals("No")) {
+                System.out.println(narrativeHost.getNarrative("enterInn"));
+                break;
+            }
+            else{
+                System.out.println("Invalid input. Please enter Yes or No.");
+            }
         }
         String name = input.nextLine();
-        System.out.println("'Ah, I'm so glad you'll be joining us, " + name + ".'");
+        System.out.println("\"Ah, I'm so glad you'll be joining us, " + name + ".\"");
         System.out.println("Devlin simply stares back at you, not writing anything down.");
         System.out.println(narrativeHost.getNarrative("devlinIntro"));
-        //late night his personality changes??
+
         System.out.println(narrativeHost.getNarrative("devlinIntro2"));
         System.out.println("What do you do now?");
-        //will need to implement error handler here as well
+
         System.out.println("Enter 1: Head upstairs to your room");
         System.out.println("Enter 2: Ask Devlin for more information about the inn");
-        int introChoice = input.nextInt();
+
         //inn backstory
-        if(introChoice == 1){
-            System.out.println(narrativeHost.getNarrative("climbStairs"));
-        }
-        else if(introChoice == 2){
-            System.out.println(narrativeHost.getNarrative("innBackstory"));
-            System.out.println(narrativeHost.getNarrative("innBackstory2"));
-            int innChoice = input.nextInt();
-            //another try catch needed
-            if (innChoice == 1){
-                System.out.println(narrativeHost.getNarrative("innBackstory3"));
-            }
-            else if (innChoice == 2) {
+        while(true){
+            String introChoice = input.nextLine();
+            if(introChoice.equals("1")){
                 System.out.println(narrativeHost.getNarrative("climbStairs"));
+                break;
+            }
+            else if(introChoice.equals("2")){
+                System.out.println(narrativeHost.getNarrative("innBackstory"));
+                System.out.println(narrativeHost.getNarrative("innBackstory2"));
+                while(true){
+                    String innChoice = input.nextLine();
+                    if (innChoice.equals("1")){
+                        System.out.println(narrativeHost.getNarrative("climbStairs"));
+                        break;
+                    }
+                    else if (innChoice.equals("2")) {
+                        System.out.println(narrativeHost.getNarrative("innBackstory3"));
+                        break;
+                    }
+                    else{
+                        System.out.println("Invalid input. Please enter 1 or 2.");
+                    }
+                }
+               break;
+            }
+            else{
+                System.out.println("Invalid input. Please enter 1 or 2.");
             }
         }
-
-
-
 
     }
 }
