f59fdf503e211ac7255f3494ff2e5b57525576de
koll-mer <e.kollmer11@gmail.com>
2022-04-13 01:17:04 -0400

    added ability to check bag for item

--- src/MistwoodInn/mainDriver.java
+++ src/MistwoodInn/mainDriver.java
@@ -1,13 +1,13 @@
 package MistwoodInn;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class mainDriver {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        ArrayList<String> inventory = new ArrayList<>();
+        ArrayList<String> bag = new ArrayList<>();
+        bag.add("Letter");
         Narrative narrativeHost = new Narrative();
         System.out.println(narrativeHost.getNarrative("intro1"));
         System.out.println(narrativeHost.getNarrative("intro2"));
@@ -17,8 +17,8 @@ public class mainDriver {
             String oldManAnswer = input.nextLine();
             if (oldManAnswer.equals("Yes")) {
                 System.out.println(narrativeHost.getNarrative("oldManCheck"));
-                inventory.add("Silver coin");
-                //System.out.println(inventory.contains("Silver coin"));
+                bag.add("Silver coin");
+                //if(bag.contains("Silver coin"));
                 System.out.println(narrativeHost.getNarrative("enterInn"));
                 break;
             }
@@ -36,6 +36,8 @@ public class mainDriver {
         System.out.println(narrativeHost.getNarrative("devlinIntro"));
 
         System.out.println(narrativeHost.getNarrative("devlinIntro2"));
+        bag.add("Room 9 Key");
+        System.out.println("Room 9 Key added to your bag");
         System.out.println("What do you do now?");
 
         System.out.println("Enter 1: Head upstairs to your room");
@@ -59,6 +61,7 @@ public class mainDriver {
                     }
                     else if (innChoice.equals("2")) {
                         System.out.println(narrativeHost.getNarrative("innBackstory3"));
+                        System.out.println(narrativeHost.getNarrative("climbStairs"));
                         break;
                     }
                     else{
@@ -71,6 +74,33 @@ public class mainDriver {
                 System.out.println("Invalid input. Please enter 1 or 2.");
             }
         }
-
+        //enter room
+        System.out.println(narrativeHost.getNarrative("turnCorner"));
+        System.out.println("What do you say back?");
+        String angeResponse = input.nextLine();
+        if (angeResponse.contains("alone") || angeResponse.contains("ALONE")){
+            System.out.println(narrativeHost.getNarrative("angeReply1"));
+        }
+        else {
+            System.out.println(narrativeHost.getNarrative("angeReply2"));
+        }
+        System.out.println(narrativeHost.getNarrative("enterRoom"));
+        System.out.println("Bag contents: " + bag);
+        //bag check
+        System.out.println("Enter the name of an item to inspect it further.");
+        System.out.println("If you are done inspecting items, enter 2.");
+        while(true){
+            String bagCheck = input.nextLine();
+            Item item1 = new Item();
+            if (bag.contains(bagCheck)){
+                System.out.println(item1.getItemInfo(bagCheck));
+            }
+            else if (bagCheck.equals("2")){
+                break;
+            }
+            else {
+                System.out.println("Incorrect input. Please enter either an item or 2.");
+            }
+        }
     }
 }
